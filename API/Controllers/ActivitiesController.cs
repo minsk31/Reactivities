911be5611490f8e245430cca3135063108ef2f2b
vdubovik-boden <v.dubovik@godeltech.com>
2023-12-07 18:47:15 +0100

    End of section 3

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
-using SQLitePCL;
 
 namespace API.Controllers
 {
